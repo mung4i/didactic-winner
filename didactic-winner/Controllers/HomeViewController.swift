0e51c06496a17bffc69d27ff212e9af1cbaf50c5
mung4i <jailbre3k@gmail.com>
2021-06-30 11:30:17 +0300

    setup tableView and navigation

--- didactic-winner/Controllers/HomeViewController.swift
+++ didactic-winner/Controllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  didactic-winner
 //
 //  Created by Martin Mungai on 30/06/2021.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
