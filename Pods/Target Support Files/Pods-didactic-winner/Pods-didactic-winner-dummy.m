e99a383e0faabe95706e2946492ef3fb12d7140b
mung4i <jailbre3k@gmail.com>
2021-06-30 09:29:44 +0300

    initial project setup, add dependencies

---
+++ Pods/Target Support Files/Pods-didactic-winner/Pods-didactic-winner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_didactic_winner : NSObject
+@end
+@implementation PodsDummy_Pods_didactic_winner
+@end
---
+++ didactic-winner/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  didactic-winner
+//
+//  Created by Martin Mungai on 30/06/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
