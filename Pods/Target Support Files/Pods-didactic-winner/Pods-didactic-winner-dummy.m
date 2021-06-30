ab305be2b349032f5c8dc09cb7938351b6bd702a
mung4i <jailbre3k@gmail.com>
2021-06-30 10:06:35 +0300

    deprecate cocoapods, preliminary folder structure

--- Pods/Target Support Files/Pods-didactic-winner/Pods-didactic-winner-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_didactic_winner : NSObject
-@end
-@implementation PodsDummy_Pods_didactic_winner
-@end
--- Pods/Target Support Files/Pods-didactic-winnerTests/Pods-didactic-winnerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_didactic_winnerTests : NSObject
-@end
-@implementation PodsDummy_Pods_didactic_winnerTests
-@end
